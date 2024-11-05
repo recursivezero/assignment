13dfcea85215535d105f56971e44f85462893a7c
VamilP <vamililporwal@gmail.com>
2024-11-05 14:29:36 +0530

    Refactored the existing files in to the archives folder

---
+++ Family-Tree/src/scripts/colors.js
@@ -0,0 +1,14 @@
+// colors.js
+export const colors = [
+    "#FFB3BA",
+    "#BAFFC9",
+    "#BAE1FF",
+    "#FFFFBA",
+    "#FFDFBA",
+    "#E0BBE4",
+    "#D2FF72",
+    "#E9EFEC",
+    "#FEC8D8",
+    "#FFDFD3",
+  ];
+  
