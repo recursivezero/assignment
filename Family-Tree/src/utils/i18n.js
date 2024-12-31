cd90513df4fa95320bcbceaa3a17a44f695351da
VamilP <vamililporwal@gmail.com>
2024-12-31 15:30:14 +0530

    Renamed the main.json file to family.json, and added the english surnames in the family.json

--- Family-Tree/src/utils/i18n.js
+++ Family-Tree/src/utils/i18n.js
@@ -2,8 +2,15 @@ import i18next from "i18next";
 import en from "../locales/en.json";
 import hindi from "../locales/hindi.json";
 
+// Determine the language: fallback to 'en' if localStorage is not available
+const savedLanguage =
+  typeof window !== "undefined" && localStorage.getItem("language")
+    ? localStorage.getItem("language")
+    : "en";
+
 i18next.init({
-  lng: "en", // Default language
+  lng: savedLanguage,
+  fallbackLng: "en",
   resources: {
     en: { translation: en },
     hindi: { translation: hindi },
